f4438d8d1c0f321cdf973a8cbc957fd61994e271
Abel Santos <santosabelgurupa@gmail.com>
2026-06-15 11:02:20 -0300

    exemplos com bootstrap

--- sindconnect/app/urls.py
+++ sindconnect/app/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from .views import home
+from . import views
 
 urlpatterns = [
     path('', home, name='home'),
+    path('socios/', views.socios, name='socios'),
+    path('exemplos/', views.exemplos, name='exemplos'),
 ]
--- sindconnect/app/views.py
+++ sindconnect/app/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 def home(request):
     pais = 'Gana'
     return render(request, 'home.html', {'pais':pais})
+
+def socios(request):
+    return render(request,'socios.html')
+
+def exemplos(request):
+    return render(request,'exemplos.html')
