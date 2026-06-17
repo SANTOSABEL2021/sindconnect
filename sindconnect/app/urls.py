bc10b07009a538dc2558b0229ebb14fcc6b4f8a1
Abel Santos <santosabelgurupa@gmail.com>
2026-06-17 20:20:07 -0300

    implementação do banco de dados no modulo socio

--- sindconnect/app/urls.py
+++ sindconnect/app/urls.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('', views.login_usuario, name='login_usuario'),
 
-
+    # PAGINAS PRINCIPAIS
     path('home-socio/', views.home_socio, name='home_socio'),
     path('home-atendente/', views.home_atendente, name='home_atendente'),
 
@@ -17,78 +15,29 @@ urlpatterns = [
     path('socio/documentos/', views.socio_documentos, name='socio_documentos'),
     path('socio/mensalidades/', views.socio_mensalidades, name='socio_mensalidades'),
     path('socio/pagamentos/', views.socio_pagamentos, name='socio_pagamentos'),
-
-
-
-
-
-
-    path('atendente/home/',views.home_atendente,name='home_atendente' ),
-
-    path('atendente/cadastro-socio/',views.cadastro_socio, name='cadastro_socio'),
-
-    path('atendente/consulta-socio/',views.consulta_socio,name='consulta_socio'),
-
-    path('atendente/usuarios/',views.usuarios, name='usuarios'),
-
-    path('atendente/dashboard/', views.dashboard, name='dashboard' ),
-
-    path('logout/',views.logout_usuario, name='logout'),
-
-
     path('socios/', views.lista_socios, name='lista_socios'),
     path('socios/editar/<int:id>/', views.editar_socio,name='editar_socio'),
     path('socios/excluir/<int:id>/', views.excluir_socio, name='excluir_socio'),
 
+    path('atendente/home/',views.home_atendente,name='home_atendente' ),
+    path('atendente/cadastro-socio/',views.cadastro_socio, name='cadastro_socio'),
+    path('atendente/consulta-socio/',views.consulta_socio,name='consulta_socio'),
+    path('atendente/usuarios/',views.usuarios, name='usuarios'),
+    path('atendente/dashboard/', views.dashboard, name='dashboard' ),
 
     # MENSALIDADES
-    path(
-        'mensalidades/',
-        views.lista_mensalidades,
-        name='lista_mensalidades'
-    ),
-
-    path(
-        'mensalidades/cadastrar/',
-        views.cadastro_mensalidade,
-        name='cadastro_mensalidade'
-    ),
-
-    path(
-        'mensalidades/editar/<int:id>/',
-        views.editar_mensalidade,
-        name='editar_mensalidade'
-    ),
-
-    path(
-        'mensalidades/excluir/<int:id>/',
-        views.excluir_mensalidade,
-        name='excluir_mensalidade'
-    ),
+    path('mensalidades/', views.lista_mensalidades, name='lista_mensalidades' ),
+    path('mensalidades/cadastrar/', views.cadastro_mensalidade, name='cadastro_mensalidade'  ),
+    path('mensalidades/editar/<int:id>/', views.editar_mensalidade, name='editar_mensalidade' ),
+    path('mensalidades/excluir/<int:id>/',views.excluir_mensalidade, name='excluir_mensalidade' ),
 
     # PAGAMENTOS
-    path(
-        'pagamentos/',
-        views.lista_pagamentos,
-        name='lista_pagamentos'
-    ),
+    path('pagamentos/',views.lista_pagamentos, name='lista_pagamentos' ),
+    path('pagamentos/cadastrar/', views.cadastro_pagamento,name='cadastro_pagamento'),
+    path('pagamentos/editar/<int:id>/',views.editar_pagamento, name='editar_pagamento' ),
+    path('pagamentos/excluir/<int:id>/', views.excluir_pagamento, name='excluir_pagamento'),
 
-    path(
-        'pagamentos/cadastrar/',
-        views.cadastro_pagamento,
-        name='cadastro_pagamento'
-    ),
-
-    path(
-        'pagamentos/editar/<int:id>/',
-        views.editar_pagamento,
-        name='editar_pagamento'
-    ),
-
-    path(
-        'pagamentos/excluir/<int:id>/',
-        views.excluir_pagamento,
-        name='excluir_pagamento'
-    ),
+    # LOGOUT
+    path('logout/', views.logout_usuario, name='logout'),
 
 ]
--- sindconnect/app/views.py
+++ sindconnect/app/views.py
@@ -1,18 +1,11 @@
 
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, Group
-
 from django.contrib.auth import logout
-
-from django.shortcuts import render, redirect, get_object_or_404
-from .models import Socio
 from .forms import SocioForm
 from django.db.models import Q
-
-from .models import Mensalidade, Pagamento
 from .forms import MensalidadeForm, PagamentoForm
 from django.shortcuts import render, redirect, get_object_or_404
 
@@ -23,55 +16,35 @@ def login_usuario(request):
     if request.method == "POST":
         usuario = request.POST.get("usuario")
         senha = request.POST.get("senha")
-
         user = authenticate(request, username=usuario, password=senha)
 
         if user is not None:
             login(request, user)
-
             if user.groups.filter(name="Socio").exists():
                 return redirect("home_socio")
-
             elif user.groups.filter(name="Atendente").exists():
                 return redirect("home_atendente")
-
             else:
                 messages.error(request, "Usuário sem grupo definido.")
                 return redirect("login")
-
         else:
             messages.error(request, "CPF/E-mail ou senha inválidos.")
-
     return render(request, "login.html")
-
-
 
 def home_atendente(request):
     return render(request, "home_atendente.html")
-
-
-# @login_required
-# def home_socio(request):
-#     return render(request, 'home_socio.html')
-
 from django.db.models import Sum
 from .models import Socio, Mensalidade, Pagamento
-
 
 @login_required
 def home_socio(request):
     socio = get_object_or_404(Socio, cpf=request.user.username)
-
     total_mensalidades = Mensalidade.objects.count()
-
     pagamentos = Pagamento.objects.filter(socio=socio)
-
     total_pagamentos = pagamentos.count()
-
     total_pago = pagamentos.aggregate(
         total=Sum('valor_pago')
     )['total'] or 0
-
     mensalidades_em_aberto = total_mensalidades - total_pagamentos
 
     return render(request, 'home_socio.html', {
@@ -82,25 +55,20 @@ def home_socio(request):
         'mensalidades_em_aberto': mensalidades_em_aberto,
     })
 
-
 @login_required
 def socio_dados(request):
     socio = get_object_or_404(Socio, cpf=request.user.username)
-
     return render(request, 'socio_dados.html', {
         'socio': socio
     })
-
 
 @login_required
 def socio_documentos(request):
     return render(request, 'socio_documentos.html')
 
-
 @login_required
 def socio_mensalidades(request):
     return render(request, 'socio_mensalidades.html')
-
 
 @login_required
 def socio_pagamentos(request):
@@ -110,26 +78,17 @@ def socio_pagamentos(request):
 def home_atendente(request):
     return render(request, 'home_atendente.html')
 
-
-
-
-
 @login_required
 def consulta_socio(request):
     return render(request, 'consulta_socio.html')
-
 
 @login_required
 def usuarios(request):
     return render(request, 'usuarios.html')
 
-
 @login_required
 def dashboard(request):
     return render(request, 'dashboard.html')
-
-
-
 
 @login_required
 def cadastro_socio(request):
@@ -154,16 +113,13 @@ def cadastro_socio(request):
             if criado:
                 user.set_password(senha_padrao)
                 user.save()
-
             grupo_socio = Group.objects.get(name='Socio')
             user.groups.add(grupo_socio)
-
             return redirect('lista_socios')
     else:
         form = SocioForm()
 
     return render(request, 'cadastro_socio.html', {'form': form})
-
 
 @login_required
 def lista_socios(request):
@@ -175,11 +131,8 @@ def lista_socios(request):
         {'socios': socios}
     )
 
-
-
 @login_required
 def editar_socio(request, id):
-
     socio = get_object_or_404(Socio, id=id)
 
     if request.method == 'POST':
@@ -192,7 +145,6 @@ def editar_socio(request, id):
         if form.is_valid():
             form.save()
             return redirect('lista_socios')
-
     else:
         form = SocioForm(instance=socio)
 
@@ -207,13 +159,9 @@ def editar_socio(request, id):
 
 @login_required
 def consulta_socio(request):
-
     pesquisa = request.GET.get('q')
-
     socios = Socio.objects.all()
-
     if pesquisa:
-
         socios = socios.filter(
             Q(nome__icontains=pesquisa) |
             Q(cpf__icontains=pesquisa)
@@ -230,11 +178,9 @@ def consulta_socio(request):
 @login_required
 def excluir_socio(request, id):
     socio = get_object_or_404(Socio, id=id)
-
     if request.method == 'POST':
         socio.delete()
         return redirect('lista_socios')
-
     return render(request, 'excluir_socio.html', {'socio': socio})
 
 @login_required
@@ -245,22 +191,18 @@ def lista_mensalidades(request):
         'mensalidades': mensalidades
     })
 
-
 @login_required
 def cadastro_mensalidade(request):
     if request.method == 'POST':
         form = MensalidadeForm(request.POST)
-
         if form.is_valid():
             form.save()
             return redirect('lista_mensalidades')
     else:
         form = MensalidadeForm()
-
     return render(request, 'cadastro_mensalidade.html', {
         'form': form
     })
-
 
 @login_required
 def editar_mensalidade(request, id):
@@ -268,27 +210,23 @@ def editar_mensalidade(request, id):
 
     if request.method == 'POST':
         form = MensalidadeForm(request.POST, instance=mensalidade)
-
         if form.is_valid():
             form.save()
             return redirect('lista_mensalidades')
     else:
+        # faz com que o formulario de cadastro sirva para editar
         form = MensalidadeForm(instance=mensalidade)
-
     return render(request, 'editar_mensalidade.html', {
         'form': form,
         'mensalidade': mensalidade
     })
 
-
 @login_required
 def excluir_mensalidade(request, id):
     mensalidade = get_object_or_404(Mensalidade, id=id)
-
     if request.method == 'POST':
         mensalidade.delete()
         return redirect('lista_mensalidades')
-
     return render(request, 'excluir_mensalidade.html', {
         'mensalidade': mensalidade
     })
@@ -306,31 +244,25 @@ def lista_pagamentos(request):
 def cadastro_pagamento(request):
     if request.method == 'POST':
         form = PagamentoForm(request.POST)
-
         if form.is_valid():
             form.save()
             return redirect('lista_pagamentos')
     else:
         form = PagamentoForm()
-
     return render(request, 'cadastro_pagamento.html', {
         'form': form
     })
 
-
 @login_required
 def editar_pagamento(request, id):
     pagamento = get_object_or_404(Pagamento, id=id)
-
     if request.method == 'POST':
         form = PagamentoForm(request.POST, instance=pagamento)
-
         if form.is_valid():
             form.save()
             return redirect('lista_pagamentos')
     else:
         form = PagamentoForm(instance=pagamento)
-
     return render(request, 'editar_pagamento.html', {
         'form': form,
         'pagamento': pagamento
@@ -340,11 +272,9 @@ def editar_pagamento(request, id):
 @login_required
 def excluir_pagamento(request, id):
     pagamento = get_object_or_404(Pagamento, id=id)
-
     if request.method == 'POST':
         pagamento.delete()
         return redirect('lista_pagamentos')
-
     return render(request, 'excluir_pagamento.html', {
         'pagamento': pagamento
     })
@@ -352,9 +282,7 @@ def excluir_pagamento(request, id):
 @login_required
 def socio_mensalidades(request):
     socio = get_object_or_404(Socio, cpf=request.user.username)
-
     mensalidades = Mensalidade.objects.all().order_by('-id')
-
     return render(request, 'socio_mensalidades.html', {
         'socio': socio,
         'mensalidades': mensalidades
@@ -363,7 +291,6 @@ def socio_mensalidades(request):
 @login_required
 def socio_pagamentos(request):
     socio = get_object_or_404(Socio, cpf=request.user.username)
-
     pagamentos = Pagamento.objects.filter(
         socio=socio
     ).order_by('-data_pagamento')
